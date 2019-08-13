f5c9242d76d86092956a7de50a224f794b37bd15
ShuaiQiNiFaGe <2418789521@qq.com>
2019-08-13 20:44:46 +0800

    aaa

--- src/main/java/com/znsd/aaa/Stu.java
+++ src/main/java/com/znsd/aaa/Stu.java
@@ -3,6 +3,13 @@ package com.znsd.aaa;
 public class Stu {
 	private String name;
 	private String pass;
+	private String email;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	public String getName() {
 		return name;
 	}
